320e6f9e324be23a6b919b750037d0eb52687551
Pasindu Vimansa Kulasooriya <38374168+InfiniteCoder96@users.noreply.github.com>
2020-04-10 15:00:02 +0530

    Backend initial commit
    Created spring boot project and configured dependancies and plugins

---
+++ Admin-Panel/Backend/src/main/java/com/gdms/goodsdeliverymanagementsystem/GoodsDeliveryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gdms.goodsdeliverymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsDeliveryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsDeliveryManagementSystemApplication.class, args);
+	}
+
+}
