67f3cc6b4cf67a5904eca01ea44e95def328c9e4
Pasindu Vimansa Kulasooriya <38374168+InfiniteCoder96@users.noreply.github.com>
2020-04-11 04:52:18 +0530

    Delivery rider app initial commit
    created spring and angular projects and configured dependecies and plugins

---
+++ Delivery-Rider-App/Backend/src/main/java/com/gdms/goodsdeliveryridermanagementsystem/GoodsDeliveryRiderManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gdms.goodsdeliveryridermanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsDeliveryRiderManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsDeliveryRiderManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Delivery-Rider-App/Backend/src/test/java/com/gdms/goodsdeliveryridermanagementsystem/GoodsDeliveryRiderManagementSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gdms.goodsdeliveryridermanagementsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoodsDeliveryRiderManagementSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
